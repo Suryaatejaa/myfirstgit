5b800d5406c24758306345f9e64bd25c7088193c
Surya Teja <ksuryatejaa57>
2022-11-24 15:01:02 +0530

    changes

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 var items = document.getElementsByClassName('list-group-item');
-console.log(items)
+items[2].getElementsByClassName.backgroundColor = 'green'
+for (var i=0; i<items.length; i++){
+    items[i].style.fontWeight = 'bold' 
+}
