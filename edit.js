8b0b3661fe21b807e7e39103bd166d113f656838
Surya Teja <ksuryatejaa57>
2022-11-26 13:50:07 +0530

    task 9

--- edit.js
+++ edit.js
@@ -1,13 +1,19 @@
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
-form.addEventListener('submit',addItem);
+form.addEventListener('submit',onSubmit);
 itemList.addEventListener('click',removeItem);
-function addItem(e){
+filter.addEventListener('keyup',filterItems);
+function onSubmit(e){
     e.preventDefault();
     var newItem = document.getElementById('item').value;
+    const descrip = document.getElementById('descrip').value;
     var li = document.createElement('li');
     li.className = 'list-group-item';
-    li.appendChild(document.createTextNode(newItem));
+    const newtext = document.createTextNode(newItem);
+    const descriptext = document.createTextNode(descrip);
+    li.appendChild(newtext);
+    li.appendChild(descriptext);
+    
     var dbutton = document.createElement('button');
     dbutton.className = 'btn btn-danger btn-sm float-right delete';
     dbutton.appendChild(document.createTextNode('X'));
@@ -22,4 +28,28 @@ function removeItem(e){
             itemList.removeChild(li);
         }
     }
+}
+function filterItems(e){
+
+    var text = e.target.value.toLowerCase();
+
+    var items = itemList.getElementsByTagName('li');
+
+    Array.from(items).forEach(function(item){
+
+        var itemName = item.firstChild.textContent; 
+        const descrip = item.childNodes[1].textContent;
+
+        if(itemName.toLowerCase().indexOf(text)!=-1 || descrip.toLowerCase().indexOf(text)!=-1) {
+
+            item.style.display = 'block';
+
+        }else{
+
+            item.style.display = "none"
+
+        }
+
+    })
+
 }
